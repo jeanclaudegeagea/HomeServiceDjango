5f7ab0f61534551c1ec006600fefea5a4ecca32f
ralph1233 <ralphmerhy93@gmail.com>
2025-03-25 16:03:37 +0200

    signup logic

--- homeservice/core/views.py
+++ homeservice/core/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserForm
 
 # Create your views here.
 
@@ -12,7 +14,33 @@ def login(request):
 
 
 def signup(request):
-    return render(request, "core/signup.html")
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            # Save the user, but don't commit yet
+            user = form.save(commit=False)
+            # Set the password to make sure it's hashed
+            user.set_password(form.cleaned_data["password"])
+            # Save the user to the database
+            user.save()
+
+            # Optionally, show a success message
+            messages.success(
+                request, "You have successfully registered! Please log in."
+            )
+
+            # Redirect to login page (or anywhere you want)
+            return redirect("login")
+        else:
+            # If the form is not valid, show errors
+            messages.error(
+                request, "There was an error in the form. Please check your inputs."
+            )
+    else:
+        # If it's a GET request, just display the form
+        form = UserForm()
+
+    return render(request, "core/signup.html", {"form": form})
 
 
 def home(request):
