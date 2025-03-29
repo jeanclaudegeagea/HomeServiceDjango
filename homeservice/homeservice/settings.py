0925ee1c3100400c6cb41a727806368a50121914
jeanclaudegeagea <jeanclaude.geagea@gmail.com>
2025-03-29 18:36:53 +0200

    Adding env and making requirements file

--- homeservice/homeservice/settings.py
+++ homeservice/homeservice/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-9-@ai2y6dn)2u^i)3lq6=rgsk25__9puknbt$pdgkv5ry_g#oo"
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -80,11 +83,11 @@ WSGI_APPLICATION = "homeservice.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "HomeService",  # Database name
-        "USER": "Test",  # Database user
-        "PASSWORD": "test123",  # Database password
-        "HOST": "localhost",  # Database host
-        "PORT": "5432",  # Database port (default is 5432)
+        "NAME": os.getenv('DB_NAME'),
+        "USER": os.getenv('DB_USER'),
+        "PASSWORD": os.getenv('DB_PASSWORD'),
+        "HOST": os.getenv('DB_HOST'),
+        "PORT": os.getenv('DB_PORT')
     }
 }
 
