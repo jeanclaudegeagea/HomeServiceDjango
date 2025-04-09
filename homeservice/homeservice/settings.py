3824e7e4fa5b42c1019508b37ac6620302d6b5f7
Jean Claude Geagea <88094662+jeanclaudegeagea@users.noreply.github.com>
2025-04-09 03:00:50 +0300

    Update settings.py

--- homeservice/homeservice/settings.py
+++ homeservice/homeservice/settings.py
@@ -29,7 +29,11 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'homeservicedjango.onrender.com',  
+    'localhost',                      
+    '127.0.0.1',                      
+]
 
 AUTH_USER_MODEL = "core.User"
 
